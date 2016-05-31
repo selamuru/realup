618639915884531d8a5740e3bed518c692f3210e
Sura Elamurugu <sura@origamilogic.com>
2016-05-30 22:27:29 -0700

    Create basic property model

--- remanage/models.py
+++ remanage/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from mongoengine import *
+from realup.settings import MONGO_DB
+import datetime
 
-# Create your models here.
+connect(MONGO_DB)
+
+class Property(Document):
+    created_at = DateTimeField(default=datetime.datetime.now, required=True)
+    updated_at = DateTimeField(default=datetime.datetime.now, required=True)
+    name = StringField(max_length=120, required=True)
+    address = StringField(max_length=500, required=True)
+    purchase_price = FloatField(required=True)
+    percentage_down = FloatField(required=True)
+    interest_rate = FloatField(required=True)
+    monthly_hoa = FloatField(required=True)
+    property_tax_rate = FloatField(required=True)
+    monthly_home_insurance = FloatField(required=True)
+    management_rate = FloatField(required=True)
+    maintenance_factor = FloatField(required=True)
+    vacancy_factor = FloatField(required=True)
+    gross_monthly_rent = FloatField(required=True)
+
+    meta = {'collection': 'properties'}
