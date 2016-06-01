75c1d6a81a41ef8fc852bfecf76a0c29a4101283
Sura Elamurugu <sura@origamilogic.com>
2016-06-01 15:15:04 -0700

    Additional calculated fields for property: monthly_mortgage, total_monthly_expenses, net_monthly_cash_flow, net_annual_cash_flow, net_annual_roi

--- remanage/models.py
+++ remanage/models.py
@@ -55,22 +55,34 @@ class Property(MyDocument):
     # calculated fields
     down_payment = DecimalField(precision=2)
     initial_loan = DecimalField(precision=2)
-    #monthly_mortgage = DecimalField(precision=2)
+    monthly_mortgage = DecimalField(precision=2)
     monthly_property_tax = DecimalField(precision=2)
-    #total_monthly_expenses = DecimalField(precision=2)
+    total_monthly_expenses = DecimalField(precision=2)
     gross_yearly_rent = DecimalField(precision=2)
-    #net_monthly_cash_flow = DecimalField(precision=2)
-    #net_annual_cash_flow = DecimalField(precision=2)
-    #net_annual_roi = FloatField()
+    net_monthly_cash_flow = DecimalField(precision=2)
+    net_annual_cash_flow = DecimalField(precision=2)
+    net_annual_roi = DecimalField(precision=2)
 
     meta = {'collection': 'properties'}
 
     def calculate_fields(self):
         self.down_payment = percentage(self.purchase_price, self.percentage_down)
         self.initial_loan = self.purchase_price - self.down_payment
+        self.monthly_mortgage = Decimal(pmt(self.initial_loan, self.interest_rate,
+                                            LOAN_TERM_MONTHS))
         self.monthly_property_tax = (percentage(self.purchase_price,
                                                 self.property_tax_rate)) / NUM_MONTHS
+
+        self.total_monthly_expenses = monthly_expenses(self.monthly_mortgage, self.monthly_hoa,
+                                                       self.monthly_property_tax,
+                                                       self.monthly_home_insurance,
+                                                       self.gross_monthly_rent,
+                                                       self.management_rate, self.vacancy_factor,
+                                                       self.maintenance_factor)
         self.gross_yearly_rent = self.gross_monthly_rent * NUM_MONTHS
+        self.net_monthly_cash_flow = self.gross_monthly_rent - self.total_monthly_expenses
+        self.net_annual_cash_flow = self.net_monthly_cash_flow * NUM_MONTHS
+        self.net_annual_roi = self.net_annual_cash_flow / self.down_payment * 100
 
     _pre_save_hooks = [
         MyDocument.calculate_timestamps,
