57a2d63e46283472ec748523952371e67616fe98
Sura Elamurugu <sura@origamilogic.com>
2016-05-31 17:48:51 -0700

    Add computation for monthly property tax and gross yearly rent

--- remanage/models.py
+++ remanage/models.py
@@ -1,9 +1,11 @@
 from mongoengine import *
 from realup.settings import MONGO_DB
-from remanage.utils import percentage
+from remanage.utils import *
 import datetime
+from decimal import *
 
 connect(MONGO_DB)
+
 
 class MyDocument(Document):
     created_at = DateTimeField(required=True)
@@ -26,40 +28,51 @@ class MyDocument(Document):
 
         super(MyDocument, self).save(*args, **kwargs)
 
+
 class User(MyDocument):
     name = StringField(required=True)
     email = StringField(required=True)
 
     meta = {'collection': 'users'}
 
+
 class Property(MyDocument):
     # required fields
     name = StringField(max_length=120, required=True)
     address = StringField(max_length=500, required=True)
-    purchase_price = FloatField(required=True)
+    purchase_price = DecimalField(required=True, precision=2)
     percentage_down = FloatField(required=True)
     interest_rate = FloatField(required=True)
-    monthly_hoa = FloatField(required=True)
-    property_tax_rate = FloatField(required=True)
-    monthly_home_insurance = FloatField(required=True)
-    management_rate = FloatField(required=True)
-    maintenance_factor = FloatField(required=True)
+    monthly_hoa = DecimalField(required=True, precision=2)
+    property_tax_rate = FloatField(required=True, precision=4)
+    monthly_home_insurance = DecimalField(required=True, precision=2)
+    management_rate = FloatField(required=True, precision=2)
+    maintenance_factor = FloatField(required=True, precision=2)
     vacancy_factor = FloatField(required=True)
-    gross_monthly_rent = FloatField(required=True)
-    user = ReferenceField(User, reverse_delete_rule=CASCADE)
+    gross_monthly_rent = DecimalField(required=True, precision=2)
+    user = ReferenceField('User', reverse_delete_rule=CASCADE, required=True)
 
     # calculated fields
-    down_payment = FloatField()
-    initial_loan = FloatField()
+    down_payment = DecimalField(precision=2)
+    initial_loan = DecimalField(precision=2)
+    #monthly_mortgage = DecimalField(precision=2)
+    monthly_property_tax = DecimalField(precision=2)
+    #total_monthly_expenses = DecimalField(precision=2)
+    gross_yearly_rent = DecimalField(precision=2)
+    #net_monthly_cash_flow = DecimalField(precision=2)
+    #net_annual_cash_flow = DecimalField(precision=2)
+    #net_annual_roi = FloatField()
+
+    meta = {'collection': 'properties'}
 
     def calculate_fields(self):
         self.down_payment = percentage(self.purchase_price, self.percentage_down)
         self.initial_loan = self.purchase_price - self.down_payment
+        self.monthly_property_tax = (percentage(self.purchase_price,
+                                                self.property_tax_rate)) / NUM_MONTHS
+        self.gross_yearly_rent = self.gross_monthly_rent * NUM_MONTHS
 
     _pre_save_hooks = [
         MyDocument.calculate_timestamps,
         calculate_fields
     ]
-
-    meta = {'collection': 'properties'}
-
