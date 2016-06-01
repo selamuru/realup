2ce464fb982176c1fceece6e90674e03611cd08f
Jonathan Lew <jdlew@Jonathans-MacBook-Pro.local>
2016-05-31 21:56:15 -0700

    Add new /portfolio route

--- realup/urls.py
+++ realup/urls.py
@@ -1,11 +1,23 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.views.generic.base import RedirectView
+
+from remanage import views as remanage_views
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'realup.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'remanage.views.index'),
+
+    # Root
+    url(r'^$', remanage_views.index),
+
+    # Portfolio route
+    url(r'^portfolio/$', remanage_views.portfolio),
+
+    # Redirect unknown paths back to root URL
+    url(r'^.*$', RedirectView.as_view(url='/', permanent=False), name='index')
 ]
--- remanage/views.py
+++ remanage/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'remanage/templates/dashboard.html')
+
+def portfolio(request):
+    return render(request, 'remanage/templates/portfolio.html')
